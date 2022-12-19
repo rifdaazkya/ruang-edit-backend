b228016c1d516c4554d6176d366b8cec65652c5e
anwargates <102929799+anwargates@users.noreply.github.com>
2022-12-19 11:46:47 +0700

    v1

--- config/database.js
+++ config/database.js
@@ -1,12 +1,26 @@
 module.exports = ({ env }) => ({
+  // remote
+  // connection: {
+  //   client: 'postgres',
+  //   connection: {
+  //     host: env('PGHOST', '127.0.0.1'),
+  //     port: env.int('PGPORT', 5432),
+  //     database: env('PGDATABASE', 'strapi'),
+  //     user: env('PGUSER', 'strapi'),
+  //     password: env('PGPASSWORD', 'password'),
+  //     ssl: env.bool(true),
+  //   },
+  // },
+
+  // local
   connection: {
     client: 'postgres',
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      database: env('PGDATABASE', 'ruang-edit-database'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', '1234'),
       ssl: env.bool(true),
     },
   },
